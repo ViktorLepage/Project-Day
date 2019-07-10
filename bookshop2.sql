74e8820c0b5f68f2d63eefd58153e387d7c288cc
Fabrizio <fabriziolucenti@gmail.com>
2019-07-10 15:58:13 +0200

    further updates

--- bookshop2.sql
+++ bookshop2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jul 10, 2019 at 12:26 PM
+-- Generation Time: Jul 10, 2019 at 01:56 PM
 -- Server version: 5.7.26
 -- PHP Version: 7.2.18
 
